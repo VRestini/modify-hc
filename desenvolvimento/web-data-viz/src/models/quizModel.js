54a39e5f30f697d00aef74a600c27e1cfd5615ad
VRestini <vitor.restini@sptech.school>
2025-05-24 16:24:05 -0300

    Feature: quiz sendo puxado do banco.

--- desenvolvimento/web-data-viz/src/models/quizModel.js
+++ desenvolvimento/web-data-viz/src/models/quizModel.js
@@ -3,7 +3,7 @@ function load(){
     var instrucaoSql = `
         select title, category, describe_quiz from quiz;
     `
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    //console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 module.exports = {
