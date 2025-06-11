7284eb0a3b82115043b6021f872cf3b15a1211d7
VRestini <vitor.restini@sptech.school>
2025-06-10 23:45:08 -0300

    Feature: biblioteca sweetalert nas validações de login e cadastro.

--- desenvolvimento/web-data-viz/public/js/login.js
+++ desenvolvimento/web-data-viz/public/js/login.js
@@ -3,12 +3,33 @@ function login() {
     var email = input_email.value
     var password = input_password.value
     if (email == "" || password == "") {
-        alert("teste")
+        Swal.fire({
+            title: "Preencha todos os campos!",
+            background: '#1a1a1a', 
+            color: '#ffffff',
+            draggable: true,
+            confirmButtonColor: '#3a0175',
+            timer: 5000
+        })
     } else if (email.indexOf("@") == -1) {
-        alert("email inválido")
+        Swal.fire({
+            title: "Email inválido!",
+            background: '#1a1a1a', 
+            color: '#ffffff',
+            draggable: true,
+            confirmButtonColor: '#3a0175',
+            timer: 5000
+        })
     }
     else if (password.length < 6) {
-        alert("Senha pequena")
+        Swal.fire({
+            title: "A senha tem que ter mais que 6 caracteres!",
+            background: '#1a1a1a', 
+            color: '#ffffff',
+            draggable: true,
+            confirmButtonColor: '#3a0175',
+            timer: 5000
+        })
     } else {
         fetch("/user/authenticate", {
             method: "POST",
